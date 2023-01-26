235d70ff151cbe88441b640a9ba9a30f84d26566
Mary Gullett <122511995+gullettmd@users.noreply.github.com>
2023-01-26 13:30:26 -0900

    Create yesnomaybe.py

---
+++ yesnomaybe.py
@@ -0,0 +1,2 @@
+"""This is a based off of the game Bagels by Al Sweigart al@inventwithpython.com. 
+For this game you guess a 4 digit number then you are given clues that tell you if you got it right."""
